6bcb22ab789d995178aa2744f2b04425ba1bf487
Vex62 <victor.lopezbe@gmail.com>
2024-03-17 17:00:36 +0100

    Overlay First Commit

--- app/src/main/java/com/example/plug_out_test_v4/ui/overlay/OverlayFragment.java
+++ app/src/main/java/com/example/plug_out_test_v4/ui/overlay/OverlayFragment.java
@@ -1,4 +1,4 @@
-package com.example.plug_out_test_v4.ui.notifications;
+package com.example.plug_out_test_v4.ui.overlay;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -12,20 +12,20 @@ import androidx.lifecycle.ViewModelProvider;
 
 import com.example.plug_out_test_v4.databinding.FragmentNotificationsBinding;
 
-public class NotificationsFragment extends Fragment {
+public class OverlayFragment extends Fragment {
 
     private FragmentNotificationsBinding binding;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        NotificationsViewModel notificationsViewModel =
-                new ViewModelProvider(this).get(NotificationsViewModel.class);
+        OverlayViewModel overlayViewModel =
+                new ViewModelProvider(this).get(OverlayViewModel.class);
 
         binding = FragmentNotificationsBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
         final TextView textView = binding.textNotifications;
-        notificationsViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        overlayViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
     }
 
--- app/src/main/java/com/example/plug_out_test_v4/ui/overlay/OverlayViewModel.java
+++ app/src/main/java/com/example/plug_out_test_v4/ui/overlay/OverlayViewModel.java
@@ -1,14 +1,14 @@
-package com.example.plug_out_test_v4.ui.notifications;
+package com.example.plug_out_test_v4.ui.overlay;
 
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-public class NotificationsViewModel extends ViewModel {
+public class OverlayViewModel extends ViewModel {
 
     private final MutableLiveData<String> mText;
 
-    public NotificationsViewModel() {
+    public OverlayViewModel() {
         mText = new MutableLiveData<>();
         mText.setValue("This is notifications fragment");
     }
